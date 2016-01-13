e9ba7727d9eebd982847a10ce2ab02dabddd4605
Jared Ballou <github@jballou.com>
2016-01-13 13:19:37 -0500

    Fixed bug where we didn't load vectors properly

--- src/Field/Vector.php
+++ src/Field/Vector.php
@@ -19,32 +19,24 @@ use Binary\Stream\StreamInterface;
  */
 class Vector extends AbstractSizedField
 {
-	var $dimensions=3;
-	// Set size to 4 bytes as a default
-	public function __construct($dim=3)
-	{
-		$this->dimensions = $dim;
-		$this->setSize(new Property(4*$this->dimensions));
-	}
     /**
      * {@inheritdoc}
      */
-    public function read(StreamInterface $stream, DataSet $result)
-    {
-        $data = $stream->read($this->size->get($result));
-
-        if (strlen($data) < 2) {
-            $data = str_pad($data, 2, "\0", STR_PAD_LEFT);
-        }
-	$vec=array();
-	for ($i=0;$i<3;$i++)
+	public function read(StreamInterface $stream, DataSet $result)
 	{
-	        $unpacked = unpack('f', $data);
-        	$this->validate($unpacked[1]);
-		$vec[$i]=$unpacked[1];
+		$vec=array();
+		for ($i=0;$i<3;$i++)
+		{
+			$data = $stream->read(4);
+			if (strlen($data) < 2) {
+				$data = str_pad($data, 2, "\0", STR_PAD_LEFT);
+			}
+			$unpacked = unpack('f', $data);
+			$this->validate($unpacked[1]);
+			$vec[$i]=$unpacked[1];
+		}
+		$result->setValue($this->name, $vec);
 	}
-        $result->setValue($this->name, $vec);
-    }
 
     /**
      * {@inheritdoc}
